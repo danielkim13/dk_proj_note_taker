795eaba5b0ba660224cbfc72965e3511507f3634
Daniel Kim <daniel.d.kim13@gmail.com>
2022-02-13 01:21:29 -0500

    done

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,42 @@
 const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
 
-const db = require("../../db/db");
+let db = require("../../db/db");
+
+// function for creating new note.
+function createNewNote(body, database) {
+  // id based on what the next index of the array will be
+  if (database.length === 0) {
+    body.id = "0";
+  } else {
+    body.id = database.length.toString();
+  }
+
+  db.push(body);
+  fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(database, null, 2));
+  return body;
+}
+
+// function for deleting the note.
+function deleteNote(id, database) {
+  // filter through array and create array with missing id.
+  database = database.filter((data) => data.id !== id);
+  fs.writeFileSync(path.join(__dirname, "../../db/db.json"), JSON.stringify(database, null, 2));
+  return database;
+}
 
 router.get("/notes", (req, res) => {
+  res.json(db);
+});
+
+router.post("/notes", (req, res) => {
+  const newNote = createNewNote(req.body, db);
+  res.json(newNote);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  db = deleteNote(req.params.id, db);
   res.json(db);
 });
 
